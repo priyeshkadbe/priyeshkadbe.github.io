079b21f632fcb3b17c379c84fb6b6893e2622cee
priyeshkadbe <priyeshkadbe49@gmail.com>
2023-02-13 18:32:26 +0530

    restructured

--- README.md
+++
@@ -1,2 +0,0 @@
-Coded in Typescript with NextJS
-[Have a look here](https://priyeshkadbe.github.io)
--- components/utils/DarkModeSwitcher.tsx
+++
@@ -1,8 +0,0 @@
-import { useThemeState } from "./Theme.provider";
-
-const DarkModeSwitcher = ({ replicated }: { replicated: boolean }) => {
-  const { isDarkMode, setIsDarkMode, theme } = useThemeState();
-
-  return <div>hello world</div>;
-};
-export default DarkModeSwitcher;
