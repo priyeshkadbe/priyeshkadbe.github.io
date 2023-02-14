db622dbd38c1011d32856ec915c2b99d138de59f
priyeshkadbe <priyeshkadbe49@gmail.com>
2023-02-14 14:58:42 +0530

    dark and light mode switcher added

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,33 +1,28 @@
 import Head from "next/head";
 import Navbar from "../Navbar";
-
+import Footer from "../Footer";
 type Props = {
   children: React.ReactNode;
-  currentPage:string;
+  currentPage: string;
 };
 
-
-function Layout({ children,currentPage }: Props){
-
-
+function Layout({ children, currentPage }: Props) {
   const pageTitle = `${
     currentPage === "Home"
       ? "Priyesh Kadbe | Portfolio"
       : `Priyesh Kadbe | ${currentPage} `
   }`;
 
-  return(
-    <div
-    >
+  return (
+    <div>
       <Head>{pageTitle}</Head>
       <>
-      <Navbar currentPage={currentPage} />
-      <div>
-        {children}
-      </div>
+        <Navbar currentPage={currentPage} />
+        <div>{children}</div>
+        <Footer />
       </>
     </div>
-  )
+  );
 }
 
 export default Layout;
