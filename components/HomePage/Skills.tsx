697e5fed7661ba240e4bbd9a217c705ffcaf5563
priyeshkadbe <priyeshkadbe49@gmail.com>
2023-02-24 18:39:49 +0530

    working on skills  page

--- components/HomePage/Skills.tsx
+++ components/HomePage/Skills.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect,useState, useRef,useContext } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import { getTechnos } from "../../utils/technos";
 import { ScrollContext } from "../Globals/ScrollObserver";
 
-
-
 function Skills() {
-   const [technos, setTechnos] = useState<string[]>([]);
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
   const { scrollY } = useContext(ScrollContext);
+
+  const technos = getTechnos(isMobile ? 40 : 90);
 
   useEffect(() => {
     const updateTechnos = () => {
-      const newTechnos = window.innerWidth > 768 ? getTechnos(80) : getTechnos(40);
-      setTechnos(newTechnos);
+      window.innerWidth > 768 ? setIsMobile(false) : setIsMobile(true);
     };
 
     updateTechnos();
@@ -20,23 +19,12 @@ function Skills() {
   }, []);
 
   return (
-    <div
-     
-      className="flex h-screen flex-col gap-y-10 relative z-10 "
-      id="skills"
-      
-    >
-      <h4
-        className="max-w-3xl font-bold text-4xl md:text-7xl mx-auto text-center my-6 tracking-widest"
-      >
+    <div className="flex h-screen flex-col gap-y-10 relative z-10 " id="skills">
+      <h4 className="max-w-3xl font-bold text-4xl md:text-7xl mx-auto text-center my-6 tracking-widest">
         MY FAVORITE EDGE TECHNOLOGIES
       </h4>
-      <div
-        
-        className="flex justify-center mx-auto max-w-md md:max-w-4xl flex-wrap"
-         
-      >
-        <div  className="flex flex-wrap">
+      <div className="flex justify-center mx-auto max-w-md md:max-w-4xl flex-wrap">
+        <div className="flex flex-wrap">
           {technos.map((techno, i) => (
             <span className="p-4" key={i}>
               {techno}
@@ -49,7 +37,3 @@ function Skills() {
 }
 
 export default Skills;
-
-
-
-
